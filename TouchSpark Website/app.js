2902a91dc4264d5ce7a3b76cfc42973bda3cbd51
torch1ca <averyarcari@gmail.com>
2024-08-03 15:37:55 -0400

    Server fully loads web-page and redirects to specified webpage
    -reorganised folder structure
      --public folder contains html include links
      --views folder contains html files
    -added package.json with nodemon
    -server redirects to proper webpage from url

--- TouchSpark Website/app.js
+++ TouchSpark Website/app.js
@@ -1,21 +1,33 @@
 const http = require('node:http');
 const fs = require('fs');
 
-const port = 8000;
+const port = 80;
 const server = http.createServer();
 
 server.on('request', (req, res) => {
-    fs.readFile('public/index.html', 'utf8', function(error, data) {
-        if(error){
-            res.writeHeader(404);
-            res.write('Error: File Not Found!');
-        }
-        else {
-            res.writeHead(200, { 'Content-Type' : 'text/html' });
-            res.write(data);
-        }
-        res.end();
-    });
+    //webpage request
+    if (req.url.split('/').length === 2) {
+        let filePath = req.url;
+        if (filePath === '/') filePath = '/index';
+        filePath = 'views' + filePath + '.html';
+        fs.readFile(filePath, 'utf8', function(err, data) {
+            if(err){
+                res.writeHeader(404);
+                res.write('Error: File Not Found!');
+                console.log(err);
+            }
+            else {
+                res.writeHead(200, { 'Content-Type' : 'text/html' });
+                res.write(data);
+            }
+            res.end();
+        });
+    }
+    //url request
+    else {
+        let file = fs.readFileSync('public' + req.url);
+        res.end(file);
+    }
 });
 
 server.listen(port, function(error) {
